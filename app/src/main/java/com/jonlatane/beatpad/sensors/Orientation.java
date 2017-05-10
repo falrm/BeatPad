aba027a3082487091538e608aca28e1ec35441fb
Jon Latane <jon.latane@smashingboxes.com>
2017-05-10 16:53:59 -0400

    Colors and orientation

--- app/src/main/java/com/jonlatane/beatpad/sensors/Orientation.java
+++ app/src/main/java/com/jonlatane/beatpad/sensors/Orientation.java
@@ -11,6 +11,7 @@ import android.hardware.SensorManager;
  */
 
 public class Orientation {
+    private static final String TAG = Orientation.class.getSimpleName();
     public static Float azimuth = 0f;
     public static Float pitch = 0f;
     public static Float roll = 0f;
@@ -39,11 +40,13 @@ public class Orientation {
     }
 
     /**
-     * Device pitch as a float between 0 and 1
+     * Device pitch as a float between 0 and 1.  Only really allows tilts up to +/-45 degrees above/below
+     * the ground.
      * @return
      */
     public static float normalizedDevicePitch() {
-        return (-pitch + 1.58f) / 3.14f;
+        float normalizedPitch = Math.max(0f, Math.min(1f, (1.58f - pitch * 2) / 3.14f));
+        return normalizedPitch;
     }
 
     /**
